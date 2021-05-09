bb9acb3473f87f544e8046472fe61d686caaad28
Lumir-Ludvik <lumirludvik@email.cz>
2021-05-09 12:36:21 +0200

    reactive variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,22 @@
 <script>
-	export let name;
+	let name ='max';
+	let age = 30;
+
+	$: uppercaseName = name.toUpperCase(); //labeled statement je to vanila JS nemusí být $ ale Svelte rozerná jen $
+	// vytvoření dynamické proměnné nemusím jí nikde deklarovat Svelte pořeší ale můžu
+	//vždy jak se změní name proměnná tak se "přepočte" uppercaseName
+
+	$: console.log(name); // vypíše name vždy jak je změna proměnné name
+
+	$: if (name === "Maximilian") {
+		age = 33; // změna age nespustí znovu tento if statement protože reaguje pouze na změnu proměnné name
+	}
+
+	const incrementAge = () => {
+		age++; // nepotřebuji this.setState() Svelte má interně odkaz na kontext
+	}
+
+	const changeName = () => name = "Maximilian"; // změním proměnnou name čímž spustím "přepočet" všech dynamických proměnných které používají name
 </script>
 
 <style>
@@ -8,4 +25,6 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {uppercaseName}!, my age is {age}</h1> <!-- podobně jak react jen nemusím označit přes this.state.name-->
+<button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change name</button>
